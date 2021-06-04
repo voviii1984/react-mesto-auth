86d78bbb8bfef06a3bd813f0a92f3cd5ed0cb487
Vladimir Lyapin <voviii1984@yandex.ru>
2021-06-04 08:32:44 +0300

    first commit

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className="footer">
-            <h2 className="footer__text">&copy; {new Date().getFullYear()} Mesto Russia</h2>
-        </footer>
-    )
-}
-
-export default Footer;
